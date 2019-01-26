72a361a734893ed472d0857594d586eee33126b2
Linda Chang <lindachang186@gmail.com>
2019-01-26 18:09:54 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,7 +5,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location
       t.string :theme
-      # t.floating :price
+      t.floating :price
       # t.boolean :family_friendly
       # t.datetime :opening_date
       # t.datetime :closing_date
