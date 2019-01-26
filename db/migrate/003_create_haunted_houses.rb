e05297e4a3ad0e2ad7da0ded55ac49859630fcb3
Linda Chang <lindachang186@gmail.com>
2019-01-26 17:57:20 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,18 @@
 # Create your haunted_houses migration here
+
+class HauntedHouses < ActiveRecord::Migration[5.1]
+
+    def change
+        create_table :haunted_houses do |t|
+            t.string :name
+            t.string :location
+            t.string :theme
+            t.floating :price
+            t.boolean :family_friendly
+            t.datetime :opening_time
+            t.datetime :closing_time
+            t.string :description
+        end
+    end
+
+end
